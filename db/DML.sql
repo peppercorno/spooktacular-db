2c24f6235c12dc518a5b060039bae17a5470506a
Dawn T <dawn.toh@gmail.com>
2023-11-05 20:07:31 +0800

    Adding queries for Review dropdown menus and improved table

--- db/DML.sql
+++ db/DML.sql
@@ -2,33 +2,46 @@
     Team Spooktacular (Team 32)
     Debbie Nguyen, Dawn Toh 
 ------------------------------*/
--- get all Price IDs, years and Base Proces to populate Admission Prices 
+
+/*------------------------------
+Admission Prices
+------------------------------*/
+-- Get all Admission Price rows
 SELECT priceID, year, basePrice FROM AdmissionPrices;
 
--- add a new admission price 
+-- Add a new Admission Price 
 INSERT INTO AdmissionPrices (year, basePrice) 
 VALUES (:yearInput, :basePriceInput);
 
--- update existing admission price
+-- Update existing Admission Price
 UPDATE AdmissionPrices SET basePrice = :newBasePriceInput 
 WHERE priceID = :priceIDToUpdate;
 
--- delete an admission price
+-- Delete an Admission Price
 DELETE FROM AdmissionPrices WHERE priceID = :priceIDToDelete;
 
--- get all Customer IDs, first names, last names and emails to populate Customers
+/*------------------------------
+Customers
+------------------------------*/
+-- Get all Customer rows to populate Customers
 SELECT customerID, firstName, lastName, email FROM Customers;
 
--- add a new Customer
+-- Get all Customer rows for dropdown menus. customerID and full name details only.
+SELECT customerID, firstName, lastName FROM Customers;
+
+-- Add a new Customer
 INSERT INTO Customers (firstName, lastName, email) VALUES (:newFirstName, :newLastName, :newEmail);
 
--- update a new Customer
+-- Update a new Customer
 UPDATE Customers SET firstName = :newFirstName, lastName = :newLastName, email = :newEmail WHERE customerID = :customerIDToUpdate;
 
--- delete a Customer
+-- Delete a Customer
 DELETE FROM Customers WHERE customerID = :customerIDToDelete;
 
--- Get all Employee IDs, first names, last names, emails, job titles, start dates, end dates, and salaries to populate Employees
+/*------------------------------
+Employees
+------------------------------*/
+-- Get all Employee rows to populate Employees
 SELECT employeeID, firstName, lastName, email, jobTitle, startDate, endDate, salary FROM Employees;
 
 -- Add a new Employee
@@ -44,7 +57,10 @@ WHERE employeeID = :employeeIDToUpdate;
 -- Delete an Employee
 DELETE FROM Employees WHERE employeeID = :employeeIDToDelete;
 
--- Get all Inventory Item IDs, room IDs, employee IDs, names, item conditions to populate Inventory Items
+/*------------------------------
+Inventory Items
+------------------------------*/
+-- Get all Inventory Item rows to populate Inventory Items
 SELECT itemID, roomID, employeeID, name, itemCondition FROM InventoryItems;
 
 -- Add a new Inventory Item
@@ -59,54 +75,73 @@ WHERE itemID = :inventoryItemIDToUpdate;
 -- Delete an Inventory Item
 DELETE FROM InventoryItems WHERE itemID = :inventoryItemIDToDelete;
 
--- Get all review information to populate Reviews
+/*------------------------------
+Reviews
+------------------------------*/
+-- Get all Review rows
 SELECT reviewID, customerID, roomID, rating, text, creationDate FROM Reviews;
 
+-- Get all Review rows with associated Customer and Room information
+SELECT Reviews.reviewID, Reviews.customerID, Reviews.roomID, Reviews.rating, Reviews.text, Reviews.creationDate, 
+    CONCAT(Customers.firstName, ' ', Customers.lastName) AS customerFullName, 
+    Rooms.name AS roomName 
+FROM Reviews 
+INNER JOIN Customers ON Customers.CustomerID = Reviews.customerID 
+LEFT JOIN Rooms ON Rooms.roomID = Reviews.roomID 
+ORDER BY creationDate DESC;
 
--- Add a new review
+-- Add a new Review
 INSERT INTO Reviews (customerID, roomID, rating, text, creationDate)
 VALUES (:customerID, :roomID, :rating, :text, NOW()); -- Use the current date and time
 
-
--- Update a review
+-- Update a Review
 UPDATE Reviews
 SET customerID = :customerID, roomID = :roomID, rating = :rating, text = :text
 WHERE reviewID = :reviewIDToUpdate;
 
--- Delete a review
+-- Delete a Review
 DELETE FROM Reviews WHERE reviewID = :reviewIDToDelete;
 
--- Get all room information to populate Rooms
+/*------------------------------
+Rooms
+------------------------------*/
+-- Get all Room rows
 SELECT roomID, name, theme, maxCapacity, level FROM Rooms;
 
+-- Get all Room rows for dropdown menus. roomID and name only.
+SELECT roomID, name FROM Rooms;
 
--- Add a new room
+-- Add a new Room
 INSERT INTO Rooms (name, theme, maxCapacity, level)
 VALUES (:name, :theme, :maxCapacity, :level);
 
-
--- Update a room
+-- Update a Room
 UPDATE Rooms
 SET name = :name, theme = :theme, maxCapacity = :maxCapacity, level = :level
 WHERE roomID = :roomIDToUpdate;
 
-
--- Delete a room
+-- Delete a Room
 DELETE FROM Rooms WHERE roomID = :roomIDToDelete;
 
--- Get all ticket information to populate Tickets
+/*------------------------------
+Tickets
+------------------------------*/
+-- Get all Ticket information to populate Tickets
 SELECT ticketID, customerID, priceID, quantity, purchaseDate, entryDate FROM Tickets;
 
--- Add a new ticket
+-- Add a new Ticket
 INSERT INTO Tickets (customerID, priceID, quantity, purchaseDate, entryDate)
 VALUES (:customerID, :priceID, :quantity, NOW(), NOW());
 
-
--- Update a ticket
+-- Update a Ticket
 UPDATE Tickets
 SET customerID = :customerID, priceID = :priceID, quantity = :quantity
 WHERE ticketID = :ticketIDToUpdate;
 
--- Delete a ticket
+-- Delete a Ticket
 DELETE FROM Tickets WHERE ticketID = :ticketIDToDelete;
+
+/*------------------------------
+InventoryItems_Employees
+------------------------------*/
 
